97ea1dd9137f5556522460c0d4a1693aa707eb4a
K.Niroshani <niroshini.07.skk@gmail.com>
2021-02-27 23:00:20 +0530

    ADD: class Beat methods & props

--- utils.js
+++ utils.js
@@ -1,35 +1,36 @@
 /**
  * Beat class that keeps track of playing the audio
  * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
- * HINT: Create a play function to play the audio if called 
+ * HINT: Create a play function to play the audio if called
  */
 class Beat {
+    constructor(audioSrc) {
+      this.audioSrc = new Audio(audioSrc)
+    }
+    playKey= () => {
+        return this.audioSrc.play();
+    }
 }
-
-
-
+let song = new Beat("./assets/Musical Orches 4.mp3")
+console.log(song.playKey());
 /**
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-    constructor(color, keyCode){
-    }
+  constructor(color, keyCode) {}
 
-    /**
-     * Set the button color based on color specified
-     */
-    setButtonColorInHTML = () => {
-    }
+  /**
+   * Set the button color based on color specified
+   */
+  setButtonColorInHTML = () => {};
 
-    /**
-     * Select function to set the background color and boxShadow
-     */
-    select = () => {
-    }
+  /**
+   * Select function to set the background color and boxShadow
+   */
+  select = () => {};
 
-    /**
-     * Deselect function to reset background color and boxShadow
-     */
-    deselect = () => {
-    }
+  /**
+   * Deselect function to reset background color and boxShadow
+   */
+  deselect = () => {};
 }
